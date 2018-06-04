e94e23e7752cfdbdd5e28c21b75909883a5af37d
Niyaz <mukhamedya.n@gmail.com>
2018-06-04 11:45:27 +0600

    finish hw

---
+++ ClientUdpShutdown/ClientUdpShutdown/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientUdpShutdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient(8888);
+
+
+            client.JoinMulticastGroup(IPAddress.Parse("235.5.5.11"), 50);
+
+            IPEndPoint remoteIp = null;
+
+            while (true)
+            {
+                byte[] data = client.Receive(ref remoteIp);
+
+                string message = Encoding.Default.GetString(data);
+
+                if (message == "ShutDown")
+                    Process.Start("shutdown", "/s /t 0");
+            }
+
+        }
+    }
+}
---
+++ ShutDownUdpServer/ShutDownUdpServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Реализовать клиент-серверное приложение, которое позволяет посредством UDP с сервера посылать все компьютерам в групповой
+ рассылке по локальной сети разослать команду отключение питания. Команда должна быть выполнена.
+     */
+
+namespace ShutDownUdpServer
+{
+    class Program
+    {
+        static IPAddress remoteAddress;
+        static void Main(string[] args)
+        {
+            remoteAddress = IPAddress.Parse("235.5.5.11");
+            UdpClient sender = new UdpClient(); 
+            IPEndPoint endPoint = new IPEndPoint(remoteAddress, 8888);
+
+            string myString = "ShutDown";
+
+            byte[] data = Encoding.Default.GetBytes(myString);
+
+            sender.Send(data, data.Length, endPoint);
+        }
+    }
+}
